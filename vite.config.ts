f188c9ef8407e767cd1157b0b4ace28f5a56c8d3
ThiagoTGM <thiagotgmgaming@gmail.com>
2026-08-19 12:17:37 +0000

    Enable async and remote functions

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,17 @@ export default defineConfig({
 		tailwindcss(),
 		sveltekit({
 			compilerOptions: {
+				experimental: {
+					async: true
+				},
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter()
+			adapter: adapter(),
+			experimental: {
+				remoteFunctions: true
+			}
 		})
 	],
 	server: {
